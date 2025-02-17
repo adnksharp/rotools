72f66c62b125435e297fc5e62c77e365d18fe5f6
adnksharp <darkmark404@protonmail.com>
2025-02-17 14:07:33 -0700

    feat: display robot

--- example.py
+++ example.py
@@ -6,15 +6,8 @@ from tabulate import tabulate
 from rotools import Nobot
 
 if __name__ == '__main__':
-    """
-   a0: float = 1.0
-   d1: float = 1.0
-   d2: float = 1.0
-   d3: float = 1.0
-   q1: float = 0.0
-   q2: float = 0.0
-   q3: float =0.0
-    """
+    title = 'robot 2DOF'
+
     d: ndarray = array([1.0, 1.0])
     Q: ndarray = array([0.0, 0.0])
     q: ndarray = array([0.0, 0.0])
@@ -38,21 +31,16 @@ if __name__ == '__main__':
     Aij = nobot.getA()
 
     links = nobot.getlinks(Aij)
-"""
-   A01: ET = ET.tz(a0) * ET.Rz()
-   A12: ET = ET.tx(d1) * ET.Rx(pi/2) * ET.Rz()
-   A23: ET = ET.tx(d2) * ET.Rz()
-   A3f: ET = ET.tx(d3) * ET.Rx(-pi/2)
- 
-   link0:Link = Link(A01,name='link0',qlim=[-pi,pi], parent=None,jindex=0,joint='R')
-   link1:Link = Link(A12,name='link1',qlim=[-pi,pi], parent=link0,jindex=1,joint='R')
-   Link2:Link = Link(A23,name='link2',qlim=[-pi,pi], parent=link1,jindex=2,joint='R')
-   ef: Link = Link(A3f, name = 'ef', parent = Link2)
- 
- 
-   links: Link = [link0, link1,Link2,ef]
 
-   robot: Robot = Robot(links,name = '3dof')
+    robot = Robot(links, name = title)
+    q[0] = pi/4
+    q[1] = pi/4
+
+    try:
+        robot.plot(q, block = True, backend = 'pyplot')
+    except KeyboardInterrupt:
+        exit(0)
+"""
    print(robot)
  
    q[0] = pi/4
@@ -67,8 +55,4 @@ if __name__ == '__main__':
    print(f'\nquat = {quat}')
    sol: tuple = robot.ik_NR(A)[0]
    print(f'\nq = {sol}')
-   try:
-        robot.plot(q, block = True, backend = 'pyplot')
-   except KeyboardInterrupt:
-        exit(0)
 """
